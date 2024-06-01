615353af4d095cd77e73354d8f6a8beaa2c5f28c
psp-xuanquy <you@example.comxquy118@gmail.com>
2024-06-01 15:16:10 +0700

    update DayNightCycle

---
+++ Assets/Scripts/DayTimeController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c80bf04d77d0d37e4d90231cb5d8d822ca1e4b4e43403a8d88d166a4720e626b
+size 2855
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:936b8e8a00a5e869ce8b1246fea40e6534cf461c9372fa4d34b34e55b9e34447
+size 274
