5a49539f51de7b40ae6937e0559748c31ee8ac61
votrongluan <trongluan115@gmail.com>
2024-06-01 11:18:41 +0700

    fix: bug in player position restoring

--- Assets/Scripts/SaveSystem/PlayerSaveSystem.cs
+++ Assets/Scripts/SaveSystem/PlayerSaveSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44e5f31938ddf9d00a713ec6eef3a4623247951657987a8bc8b7256fd5c59675
-size 402
+oid sha256:f6ca4fd09e08e5a2aa3bc115c2e603867163e0db70a6a44ca39ed125fa507967
+size 721
