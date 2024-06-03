f07efe1147123c17562e97801541c02911545469
Daiyun199 <yenphung3000@gmail.com>
2024-06-03 08:28:29 +0700

    add more plant

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c9e47ad2348b011ea244235ffdc554ff4cedaf7ee5f0abf55c4f93c69ad7cd8
-size 6178
+oid sha256:25e54f9801f7d01770d10e9a7137f8761c516db6162f975e137368d321a63d9a
+size 6578
--- Assets/Scripts/PlantingSystem.cs
+++ Assets/Scripts/PlantingSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbec2e6cf9a0e8a026655ba2e9962155ba00e48207a191d9ea57f88f28146bc7
-size 11768
+oid sha256:4e485484634e0850192d0f58b2e36368b5dec662d3f6435eb8b9f632a873e5bd
+size 14023
