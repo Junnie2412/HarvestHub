54f965e2d27f7e0b7ff12c28193c4dea07820331
Daiyun199 <yenphung3000@gmail.com>
2024-06-03 07:42:11 +0700

    create water plant

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5339c9a5e72cfa6ceb7353cf1a1fb0b94517492f996ee2de9904e0a06a543651
-size 6044
+oid sha256:2c9e47ad2348b011ea244235ffdc554ff4cedaf7ee5f0abf55c4f93c69ad7cd8
+size 6178
--- Assets/Scripts/PlantingSystem.cs
+++ Assets/Scripts/PlantingSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a74b20a772d5acd109131e986177389c4363afa281e82a0cfdaff213cc874f58
-size 9482
+oid sha256:dbec2e6cf9a0e8a026655ba2e9962155ba00e48207a191d9ea57f88f28146bc7
+size 11768
