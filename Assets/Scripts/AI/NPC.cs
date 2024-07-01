bcb6732e2d617334fcbf15e4020a79da868d0c5e
votrongluan <115349873+votrongluan@users.noreply.github.com>
2024-07-02 00:03:53 +0700

    fix: coin received after finish task

--- Assets/Scripts/AI/NPC.cs
+++ Assets/Scripts/AI/NPC.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdb52de892e625ecd34a8f5c24b0639d93583b70816d7427ec57f1107aef5720
-size 6585
+oid sha256:460ec8e1fd0d3db2f8de31583db9d089e4cae940da93a8c42a63717a5c500e70
+size 6729
--- Assets/Scripts/Cheat/Cheat.cs
+++ Assets/Scripts/Cheat/Cheat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abf970a84827e63a5ba9fb30e452073d8a0bba9dd02911e0dbf7aaa8a1ec99e3
-size 540
+oid sha256:46195d4bef0676e59ee9c143e6446a0ddf7b2ffc664ed752976103c266c42992
+size 541
