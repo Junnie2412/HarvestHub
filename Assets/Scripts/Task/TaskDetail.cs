a93ff62756a02760bd0025047fe0307434370cc3
votrongluan <115349873+votrongluan@users.noreply.github.com>
2024-07-01 23:29:30 +0700

    fix: task title and chick sort order and minor change task detail

--- Assets/Scripts/Task/TaskDetail.cs
+++ Assets/Scripts/Task/TaskDetail.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62043ef17dfcafd0f80b8560bcc1ddd56f5512c426ad14a0e5d4b90ba7507129
+oid sha256:5bfce6651e21612ceb5494977d54ebf3f362060521c1d5887a1034c6de78e8d9
 size 3181
